a22355deb7aa39ce54acb5b4a79cf55df5ea6795
corey gibson <cgibsonmm@gmail.com>
2020-05-30 17:53:56 -0400

    test

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const PORT = process.env.PORT || 3000;
 const morgan = require("morgan");
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 let api = axios.create({
@@ -22,6 +24,8 @@ app.get("/res", (req, res) => {
 
 app.post("/sms", (req, res) => {
   console.log(req.body);
+
+  res.end();
 });
 
 let count = 0;
